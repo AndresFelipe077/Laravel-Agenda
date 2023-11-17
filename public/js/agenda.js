d7bd65e690489d565d0dabd3112c9cea45a76517
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-17 17:53:55 -0500

    First commit

---
+++ public/js/agenda.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var calendarEl = document.getElementById('agenda');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+
+    calendar.render();
+    
+})
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/evento', [EventoController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
