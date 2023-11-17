acc2f10cae5c9c2022d99b34cae1ab9d3a25513d
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-17 18:32:15 -0500

    Create form

--- resources/views/evento/index.blade.php
+++ resources/views/evento/index.blade.php
@@ -20,11 +20,47 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <form action="">
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="id" name="id"
+                                placeholder="Id del evento">
+                            <label for="id">Id</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="title" name="title"
+                                placeholder="Escribe el titulo del evento">
+                            <label for="title">Titulo del evento</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <textarea class="form-control" name="descripcion" placeholder="Descripción del evento" id="description"
+                                style="height: 100px"></textarea>
+                            <label for="description">Descripción</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="date" class="form-control" name="start" id="start"
+                                placeholder="Fecha inicial">
+                            <label for="start">Fecha inicial</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input type="date" class="form-control" name="end" id="end"
+                                placeholder="Fecha final">
+                            <label for="end">Fecha final</label>
+                        </div>
+
+                    </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+
+                  <button type="button" class="btn btn-success" id="btnGuardar">Guardar</button> 
+                  <button type="button" class="btn btn-warning" id="btnModificar">Modificar</button> 
+                  <button type="button" class="btn btn-danger"  id="btnEliminar">Eliminar</button> 
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
                 </div>
             </div>
         </div>
