68c7f21277757da69761d712a86d37e7e47f457a
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-17 18:17:58 -0500

    Add modal

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('agenda');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        locale:"es",
+
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,listWeek'
+        }
+
     });
 
     calendar.render();
--- resources/views/evento/index.blade.php
+++ resources/views/evento/index.blade.php
@@ -1,10 +1,32 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="container">
         <div id="agenda">
             calendario
         </div>
     </div>
 
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#evento">
+        Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="evento" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
